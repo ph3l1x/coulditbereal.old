ecdf9a4104c7b9989458dfc9516d8b35bba29164
ph3l1x <rperry@mindimage.net>
2015-09-19 23:33:23 -0600

    Added structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coulditberealApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coulditberealApp
+ */
+angular.module('coulditberealApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coulditberealApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coulditberealApp
+ */
+angular.module('coulditberealApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
