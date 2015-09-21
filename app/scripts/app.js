308568145c07af0c8ecc3b85bfce2fa6d54a6032
ph3l1x <rperry@mindimage.net>
2015-09-20 23:55:50 -0600

    Added a few basic pages and have registration form with validation working with debugging errors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,11 +24,26 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl',
+        controllerAs: 'register'
+        })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+        })
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,63 @@
  * Controller of the coulditberealApp
  */
 angular.module('coulditberealApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+    .controller('ContactCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+    .controller('LoginCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+    .controller('AboutCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+    .controller('RegisterCtrl', function ($scope) {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        $scope.master = {};
+        $scope.update = function (user) {
+            $scope.master = angular.copy(user);
+        };
+        $scope.reset = function () {
+            $scope.user = angular.copy($scope.master);
+        };
+        $scope.reset();
+        $scope.message = "FUCK OFF";
+
+    })
+.directive('validateEquals', [function() {
+    return {
+        require: 'ng-model',
+        link: function(scope, elem, attrs, ctrl) {
+            var password2 = '#' + attrs.validateEquals;
+            console.log(attrs);
+            elem.add(password2).on('focusout', function() {
+                scope.$apply(function() {
+                    var v = elem.val()===$(password1).val();
+                    ctrl.$setValidity('inputmatches', v);
+                });
+            });
+        }
+    };
+}]);
